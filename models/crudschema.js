9a21006cdcbc8d795400df564a1355fc8d4d0e88
Dhaval Kapupara <dhaval@fivestake.com>
2017-03-18 13:15:50 +0530

    Add files via upload

---
+++ models/crudschema.js
@@ -0,0 +1,24 @@
+﻿var mongoose = require('mongoose');
+var assert = require('assert');
+var Schema = mongoose.Schema;
+var CrudSchema = new Schema({
+    firstname: {
+        type: String,
+        require: true
+    },
+    mobileno: {
+        type: String,
+        required: true
+       
+    },
+    email: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+}, {
+    timestamps: true //Add two fields automatically createdAt and updatedAt
+});
+var Crud = mongoose.model('Crud', CrudSchema);
+module.exports = Crud;

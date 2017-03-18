676adeac24606370167064f47a8177f9488f81bc
Dhaval Kapupara <dhaval@fivestake.com>
2017-03-18 16:36:35 +0530

    Update expenserouter.js

--- routes/expenserouter.js
+++ routes/expenserouter.js
@@ -1,4 +1,4 @@
-﻿var express = require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Expense = require('../models/expenseschema');
@@ -26,6 +26,7 @@ expenseRouter.route('/')
 expenseRouter.route('/remove')
 .post(function (req, res) {
     var id = req.body.id;
+
     Expense.remove({ _id: id }, function (err, doc) {
         if (err) res.json({ "status": "fail", "message": "Something went wrong please try again" });
         res.json({ "status": "success", "message": "Expense Deleted Successfully" });
@@ -36,30 +37,34 @@ expenseRouter.route('/remove')
 expenseRouter.route('/getbyid')
 .post(function (req, res) {
     var id = req.body.id;
-    Expense.findById(id, function (err, expense) {
-        if (err) res.json({ "status": "fail", "message": "Something went wrong please try again" });
-        res.json({ "status": "success", "message": "get by id", "Expense": expense });
-    });
+    try {
+        Expense.findById(id, function (err, expense) {
+            if (err) res.json({ "status": "fail", "message": "Something went wrong please try again" });
+            res.json({ "status": "success", "message": "get by id", "Expense": expense });
+        });
+    }
+    catch (err) {
+        res.json({ "status": "fail", "message": "Something went wrong please try again", "Expense": "" });
+    }
 });
 expenseRouter.route('/edit')
 .post(function (req, res) {
     var id = req.body.id;
-    var expdate = req.body.date;
-    var amount = req.body.amount;
+    var date = req.body.date;
     var project = req.body.project;
-    var extype = req.body.extype;
+    var head = req.body.head;
     var comment = req.body.comment;
-
+    var amount = req.body.amount;
     Expense.findById(id, function (err, expense) {
         if (err) {
             res.json({ "status": "fail", "message": "Something went wrong please try again" });
         }
         else {
-            expense.date = expdate;
-            expense.amount = amount;
+            expense.date = date;
             expense.project = project;
-            expense.extype = extype;
+            expense.head = head;
             expense.comment = comment;
+            expense.amount = amount;
 
             expense.save(function (err, expense) {
                 if (err) {

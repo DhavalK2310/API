ff216e27a47797c99d097917897a30082e8b1028
Dhaval Kapupara <dhaval@fivestake.com>
2017-03-20 11:46:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ db.on('open',function(){
 
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -51,7 +50,11 @@ app.use('/expense', expenseRouter)
 app.use('/booking', bookingRouter)
 app.use('/crud', crudRouter)
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

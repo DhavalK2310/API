55729b738fcf7424546ab0f898cb7a17fd8c9742
Dhaval Kapupara <dhaval@fivestake.com>
2017-03-20 16:26:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,19 +32,6 @@ db.on('open',function(){
 var app = express();
 // view engine setup
 
-allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  if ('OPTIONS' === req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-};
-
-app.use(allowCrossDomain);
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
